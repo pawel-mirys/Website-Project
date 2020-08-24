979e24bfac91c1fff23f702140f370938e5da931
YoshiCity <pawel.mirys@gmail.com>
2020-08-24 17:10:57 +0200

    Burger menu toggle

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 let controller;
 let slideScene;
 let pageScene;
-let mouse = document.querySelector('.cursor');
-let mouseTxt = mouse.querySelector('span');
+
 
 function animatesSlides() {
     // Init Controller
@@ -48,12 +47,15 @@ function animatesSlides() {
             .setPin(slide, { pushFollowers: false })
             .setTween(pageTl)
             .addTo(controller)
-
     });
 }
 
-function cursor(e) {
 
+const mouse = document.querySelector('.cursor');
+const mouseTxt = mouse.querySelector('span');
+const burger = document.querySelector('.burger');
+
+function cursor(e) {
     mouse.style.top = e.pageY + 'px';
     mouse.style.left = e.pageX + 'px';
 }
@@ -76,6 +78,26 @@ function activeCursor(e) {
     }
 }
 
+function navToggle(e) {
+    if (!e.target.classList.contains('active')) {
+        e.target.classList.add('active');
+        gsap.to('.line1', 0.5, { rotate: "45", y: 5, background: "black" });
+        gsap.to('.line2', 0.5, { rotate: "-45", y: -5, background: "black" });
+        gsap.to('#logo', 1, { color: "black" })
+        gsap.to('.nav-bar', 1, { clipPath: 'circle(2500px at 100% -10%)' });
+        document.body.classList.add('hide');
+    } else {
+        e.target.classList.remove('active');
+        gsap.to('.line1', 0.5, { rotate: "0", y: 0, background: "white" });
+        gsap.to('.line2', 0.5, { rotate: "0", y: 0, background: "white" });
+        gsap.to('#logo', 1, { color: "white" })
+        gsap.to('.nav-bar', 1, { clipPath: 'circle(50px at 100% -10%)' });
+        document.body.classList.remove('hide');
+    }
+}
+
+// Event listeners
+burger.addEventListener('click', navToggle);
 window.addEventListener('mousemove', cursor);
 window.addEventListener('mouseover', activeCursor);
 
