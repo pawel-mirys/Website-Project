e4680b67c0da98882781f84c41471fb428010130
YoshiCity <pawel.mirys@gmail.com>
2020-08-26 17:34:00 +0200

    Page transitions

--- app.js
+++ app.js
@@ -96,9 +96,53 @@ function navToggle(e) {
     }
 }
 
+// Barba page transitions
+const logo = document.querySelector('#logo');
+barba.init({
+    views: [{
+            namespace: 'home',
+            beforeEnter() {
+                animatesSlides();
+                logo.href = './index.html';
+            },
+            beforeLeave() {
+                slideScene.destroy();
+                pageScene.destroy();
+                controller.destroy();
+            }
+        },
+        {
+            namespace: 'fashion',
+            beforeEnter() {
+                logo.href = '../index.html';
+                gsap.fromTo('.nav-header', 1, { y: '100%' }, { y: '0%', ease: 'power2.inOut' })
+            }
+        }
+    ],
+    transitions: [{
+        leave({ current, next }) {
+            let done = this.async();
+            // animation
+            const tl = gsap.timeline({ defaults: { ease: 'power2.inOut' } });
+            tl.fromTo(current.container, 1, { opacity: 1 }, { opacity: 0 });
+            tl.fromTo('.swipe', 0.75, { x: '-100%' }, { x: '0%', onComplete: done }, '-=0.5');
+
+        },
+        enter({ current, next }) {
+            let done = this.async();
+            // Scroll to the top
+            window.scrollTo(0, 0);
+            // animation
+            const tl = gsap.timeline({ defaults: { ease: 'power2.inOut' } });
+            tl.fromTo('.swipe', 1, { x: '0%' }, { x: '100%', stagger: 0.25, onComplete: done });
+            tl.fromTo(next.container, 1, { opacity: 0 }, { opacity: 1 });
+
+        }
+    }]
+})
+
+
 // Event listeners
 burger.addEventListener('click', navToggle);
 window.addEventListener('mousemove', cursor);
 window.addEventListener('mouseover', activeCursor);
-
-animatesSlides();
